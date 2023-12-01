9c7a3b1116841daf5833d5e52f0e2d24ffa37312
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-12-01 16:51:08 -0300

    adding initial comments and code base

--- BinPacking/Program.cs
+++ BinPacking/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] values = [500, 300, 400, 700];
+int capacity = 1000;
+
+int n = amountBins(values, capacity);
+
+Console.WriteLine(n);
+
+var results = BinPacking(values, capacity);
+
+foreach (var (prd1, prd2, tot) in results)
+{
+    Console.WriteLine("{0} + {1} = {2}", prd1, prd2, tot);
+}
+
+static int amountBins(int[] itemSizes, int binSize)
+{
+    int bins = itemSizes.Sum() / binSize;
+    return (itemSizes.Sum() % binSize != 0) ? ++bins : bins;
+}
+
+static List<(int prd1, int prd2, int tot)> BinPacking(int[] itemSizes, int binSize)
+{
+    List<(int prd1, int prd2, int tot)> results = [];
+
+    for (int i = 0; i < itemSizes.Length; i++)
+    {
+        for (int j = i + 1; j < itemSizes.Length; j++)
+        {
+            if (itemSizes[i] + itemSizes[j] <= binSize)
+            {
+                results.Add((itemSizes[i], itemSizes[j], itemSizes[i] + itemSizes[j]));
+            } else if (itemSizes[i] <= binSize) {
+                results.Add((itemSizes[i], 0, itemSizes[i]));
+            }
+        }
+    }
+
+    results.Sort((a, b) => b.tot - a.tot);
+
+    return results;
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Bin Packing
+
+Para a fórmula matemática temos:
+
+```
+n = (∑wi / b) + 1
+```
+
+Onde `n` recebe o número de bins que precisamos para `∑wi` somatoria de wi dividido por `b` que é a capaciadade da bin com a soma de mais uma.
+
+Vamos para um exemplo:
+
+wi = [1,2,3,4]
+
+b = 5
+
+n = (10 / 5) + 1
+
+n = 3
+
+Para esse caso acima não seria necessário a caixa adicional então resolvi melhorar um pouco a fórmula matemática para tratar esse ponto.
+
+```
+n = (∑wi / b)
+
+n = (∑wi % b) != 0 ? ++n : n
+```
+
+```csharp
+static int amountBins(int[] itemSizes, int binSize)
+{
+    int bins = itemSizes.Sum() / binSize;
+    return (itemSizes.Sum() % binSize != 0) ? ++bins : bins;
+}
+```
